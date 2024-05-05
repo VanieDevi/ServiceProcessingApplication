2167bce9655270fdeee54f6577cfc60149500a19
VANIE SRINIVASAN <30044294@tafe.wa.edu.au>
2024-05-05 20:13:33 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceProcessingApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ServiceFinished_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delivered_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Regular_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Express_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SvcQueue_ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        private void FinishedQueue_ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServiceItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServiceProcessingApplication
+{
+    public class ServiceItem
+    {
+        private String ClientName;
+        private String DroneModel;
+        private String ServiceProblem;
+        private Double ServiceCost;
+        private String ServiceTag;
+    }
+}
